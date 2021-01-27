c4e23ab08a1a397699bf29bfe329817480c3464c
Josh Elster <josh@liquidelectron.com>
2021-01-26 21:47:45 -0600

    including prod webpack config in updates

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,8 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const path = require('path');
-const fs = require('fs');
 
-module.exports = env => {
-    const prodConfig = {
-        mode: "production",
-        devtool: "source-map"
-    };
-    return merge(common, prodConfig);
+const prodConfig = {
+    mode: "production",
+    devtool: "source-map"
 };
+module.exports = merge(common, prodConfig);
