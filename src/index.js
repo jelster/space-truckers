71d921324deb8d037d7550fefd907054ef12fc26
Josh Elster <josh@liquidelectron.com>
2021-01-29 10:11:24 -0600

    Adapt start scene snippet into Class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { Engine } from "@babylonjs/core";
-
+import { Engine, Scene } from "@babylonjs/core";
+import StartScene from "./startscene";
 const CanvasName = "index-canvas";
 
 let canvas = document.createElement("canvas");
@@ -8,4 +8,8 @@ canvas.style.width = "100%";
 canvas.style.height = "100%";
 document.body.appendChild(canvas);
 
-export {canvas};
+let eng = new Engine(canvas, true, null, true);
+let startScene = new StartScene(eng);
+eng.runRenderLoop(() => {
+    startScene.scene.render();
+});
