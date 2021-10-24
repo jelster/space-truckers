26a68264f68a3709b3fefcc356309da2ded9517a
Josh Elster <josh@liquidelectron.com>
2021-10-24 12:49:41 -0500

    add driving config, Radar Procedural Texture

---
+++ src/driving/gameData.js
@@ -0,0 +1,42 @@
+import truckModelUrl from './assets/models/Semi truck.glb';
+import envTextureUrl from './assets/environment/milkyway-pbr-hdr.env';
+
+import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+
+const screenConfig = {
+    GUI_MASK: 0x2,
+    SCENE_MASK: 0x1,
+    radarTextureResolution: 4096,
+    guiViewportSize: 4.96,
+    followCamSetup: {
+        lowerAlphaLimit: Math.PI,
+        upperAlphaLimit: 5.600,
+        lowerBetaLimit: 0,
+        upperBetaLimit: 1.5000,
+        lowerRadiusLimit: 25.0,
+        upperRadiusLimit: 180.0
+    }
+};
+const truckSetup = {
+    modelUrl: truckModelUrl,
+    modelScaling: 0.02,
+    modelName: "semi_truck.1",
+    turnSpeedRadians: Math.PI / 270,
+    maxAcceleration: 9.86,
+    initialVelocity: new Vector3(0, 0, 20),
+    physicsConfig: {
+        mass: 5000,
+        restitution: 0.1,
+        damping: 0.1,
+        friction: 0.00002
+    }
+};
+
+const environmentConfig = {
+    envTextureUrl: envTextureUrl,
+    skyBoxSize: 20000,
+
+
+}
+
+export default { screenConfig, truckSetup };
