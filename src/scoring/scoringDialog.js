5d0c3fd4fc005fc69993e8419385e0e0be1736e9
Josh Elster <josh@liquidelectron.com>
2021-12-18 15:28:36 -0600

    extract score text block creation to fn

--- src/scoring/scoringDialog.js
+++ src/scoring/scoringDialog.js
@@ -5,6 +5,28 @@ import { Control } from "@babylonjs/gui/2D/controls/control";
 import { Scalar } from "@babylonjs/core/Maths/math.scalar";
 import DialogBox from "../guis/guiDialog";
 
+const sampleScore = {
+    // scoreFactors: {
+    //     routeLength: 12450.25,
+    //     cargoCondition: 0.768,
+    //     encounters: 125
+    // },
+    // multipliers: {
+    //     transitTime: 1.2, //{ expected: 180, actual: 150, factor: 1.2  },
+    //     delivery: 1.0,
+    //     condition: 0.768,
+    //     encounterTypes: 1.05
+    // },
+    finalScores: {
+        'Base Delivery': 1000,
+        'Route Score': 14940,
+        'Cargo Score': 11474,
+        'Delivery Bonus': 10000,
+        'Encounters': 1312,
+        'Final Total': 38726
+    }
+};
+
 function* nextColor() {
     while (true) {
         yield "#0d5088";
@@ -30,8 +52,8 @@ function createScoringDialog(advancedTexture, scoreData, scene) {
     let dialog = { scoreDialog };
     dialog.height = "98%";
     let scoringCo = scoringAnimationCo();
-    scoreDialog.onDisplayChangeComplete.addOnce(() =>
-        scene.onBeforeRenderObservable.runCoroutineAsync(scoringCo))
+    // scoreDialog.onDisplayChangeComplete.addOnce(() =>
+    scene.onBeforeRenderObservable.runCoroutineAsync(scoringCo);
 
     function* scoringAnimationCo() {
         let { finalScores } = scoreData;
@@ -41,28 +63,17 @@ function createScoringDialog(advancedTexture, scoreData, scene) {
         let computedHeight = 0;
         for (i in finalScores) {
             yield Tools.DelayAsync(500);
+
             let frameCounter = 0
             let label = i;
             let score = Number(finalScores[i]);
-            let scoreBlock = new TextBlock("scoreLine", `${label}`);
-            scoreBlock.width = "100%";
-            scoreBlock.color = colorPicker.next().value;
-            scoreBlock.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
-
-            let isFinalScore = label.toLowerCase().includes("final");
-            if (isFinalScore) {
-                scoreBlock.height = "104px";
-                scoreBlock.transformCenterX = scoreBlock.transformCenterY = 0;
-                scoreBlock.scaleX = scoreBlock.scaleY = 1.05;
-            }
-            else {
-                scoreBlock.height = "52px";
-            }
+            let scoreBlock = createScoringBlock(label);
             computedHeight += scoreBlock.heightInPixels;
             bodyStack.addControl(scoreBlock);
             bodyStack.heightInPixels = computedHeight;
             scrollBar.value = scrollBar.maximum;
             yield Tools.DelayAsync(1800);
+
             let skipCountUp = label.toLowerCase().includes("bonus");
             if (skipCountUp) {
                 scoreBlock.text = `${label}.........${score.toFixed().toLocaleString()}`;
@@ -82,6 +93,23 @@ function createScoringDialog(advancedTexture, scoreData, scene) {
             yield;
         }
         return;
+
+        function createScoringBlock(label) {
+            let scoreBlock = new TextBlock("scoreLine", `${label}`);
+            scoreBlock.width = "100%";
+            scoreBlock.color = colorPicker.next().value;
+            scoreBlock.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+            let isFinalScore = label.toLowerCase().includes("final");
+            if (isFinalScore) {
+                scoreBlock.height = "104px";
+                scoreBlock.transformCenterX = scoreBlock.transformCenterY = 0;
+                scoreBlock.scaleX = scoreBlock.scaleY = 1.05;
+            }
+            else {
+                scoreBlock.height = "52px";
+            }
+            return scoreBlock;
+        }
     }
 }
 
